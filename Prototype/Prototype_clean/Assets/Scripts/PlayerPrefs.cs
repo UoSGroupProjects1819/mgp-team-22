51dbf092726627b98041b4b568e4f75f7195b39d
Joshua Donnell <43639286+Sawkss@users.noreply.github.com>
2019-02-27 12:39:48 +0000

    PlayerPrefs (WIP)

---
+++ Prototype/Prototype_clean/Assets/Scripts/PlayerPrefs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerPrefs : MonoBehaviour
+{
+    int Value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("Score", 1);
+        SetText();
+    }
+
+    void SetText()
+    {
+       Value = PlayerPrefs.GetInt("Value", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Value = +1;
+
+        }
+    }
+}
