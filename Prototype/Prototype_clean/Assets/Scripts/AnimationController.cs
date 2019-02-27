f86685202654edb6253f712ee557434937080b57
Alex Mednick <32356860+err0rzzz@users.noreply.github.com>
2019-02-27 17:49:27 +0000

    rotation for player charater

---
+++ Prototype/Prototype_clean/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public static bool isRight;
+    float horizInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizInput = Input.GetAxis("Horizontal");
+
+        if (horizInput > 0) isRight = true;
+        if (horizInput < 0) isRight = false;
+    }
+}
