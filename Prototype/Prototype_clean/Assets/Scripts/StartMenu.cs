f9deb28a106a08bb0c612272730db88559da2137
Joshua Donnell <43639286+Sawkss@users.noreply.github.com>
2019-03-20 15:55:38 +0000

    Start Menu (WIP)

---
+++ Prototype/Prototype_clean/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject MenuCanvas;
+    public Button NewGameBut;
+    public Button LoadGameBut;
+    public Button ExitBut;
+
+    private GameManager gameMan;
+    public string ArtifactTrigger1, ArtifactTrigger2;
+    public void SetGameManager(GameManager game)
+    {
+        gameMan = game;
+    }
+
+    public void NewGame()
+    {
+        PlayerPrefs.SetInt(ArtifactTrigger1, 0);
+        PlayerPrefs.SetInt(ArtifactTrigger2, 0);
+        PlayerPrefs.SetString("SpawnTarget", "Home");
+        SceneManager.LoadScene("World1");
+
+    }
+
+
+    
+
+    
+
+
+
+
+
+
+}
