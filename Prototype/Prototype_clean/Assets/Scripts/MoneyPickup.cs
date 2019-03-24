4b40cadd47219181a98a3d1a37365d704e417adb
Joshua Donnell <43639286+Sawkss@users.noreply.github.com>
2019-03-24 11:26:01 +0000

    Money Pickup Script created

---
+++ Prototype/Prototype_clean/Assets/Scripts/MoneyPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyPickup : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("Money"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
