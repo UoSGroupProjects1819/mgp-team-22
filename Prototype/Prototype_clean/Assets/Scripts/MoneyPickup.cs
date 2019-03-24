80acfa22781d09aab72872faa7e0fc1180f7a66f
Joshua Donnell <43639286+Sawkss@users.noreply.github.com>
2019-03-24 12:31:39 +0000

    Updated Pickup script to count each coin picked up

--- Prototype/Prototype_clean/Assets/Scripts/MoneyPickup.cs
+++ Prototype/Prototype_clean/Assets/Scripts/MoneyPickup.cs
@@ -1,15 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MoneyPickup : MonoBehaviour
+    
 {
-    void OnTriggerEnter2D(Collider2D other)
+    public GameManager gameMan;
+    public Text MoneyText;
+    public int MoneyCount;
+
+    void OnTriggerEnter2D(Collider2D trigger)
     {
-        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
-        if (other.gameObject.CompareTag("Money"))
+        //Check the provided Collider2D parameter other to see if it is tagged "Money", if it is...
+        if (trigger.gameObject.CompareTag("Money"))
         {
-            other.gameObject.SetActive(false);
+            trigger.gameObject.SetActive(false);
+
+            MoneyCount = MoneyCount + 1;
+
+            SetMoneyText();
         }
+    }
+
+    void SetMoneyText()
+    {
+        MoneyText.text = "Coins: " + MoneyCount.ToString();
     }
 }
