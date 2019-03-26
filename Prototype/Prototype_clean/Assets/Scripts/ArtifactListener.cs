70d839d914f29397879b558bf2efcba188a6ace4
sean-turner1 <36117254+sean-turner1@users.noreply.github.com>
2019-03-26 01:31:45 +0000

    ArtifactListener uses arrays of objects now, runs in  start not update

--- Prototype/Prototype_clean/Assets/Scripts/ArtifactListener.cs
+++ Prototype/Prototype_clean/Assets/Scripts/ArtifactListener.cs
@@ -6,7 +6,7 @@ public class ArtifactListener : MonoBehaviour
 
 
 {
-    public GameObject ToggleElements1, ToggleElements2;
+    public GameObject[] ToggleElements1, ToggleElements2;
     private int worldValue1, worldValue2;
 
     private Vector3 SpawnLocation;
@@ -16,19 +16,32 @@ public class ArtifactListener : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        worldValue1 = PlayerPrefs.GetInt(Toggle1);
+        worldValue2 = PlayerPrefs.GetInt(Toggle2);
 
+        foreach (GameObject element in ToggleElements1)
+        {
+            if(worldValue1 == 1) element.SetActive(true);
+            if (worldValue1 == 0) element.SetActive(false);
+        }
+
+        foreach (GameObject element in ToggleElements2)
+        {
+            if (worldValue1 == 1) element.SetActive(true);
+            if (worldValue1 == 0) element.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        worldValue1 = PlayerPrefs.GetInt(Toggle1);
-        worldValue2 = PlayerPrefs.GetInt(Toggle2);
+        //worldValue1 = PlayerPrefs.GetInt(Toggle1);
+        //worldValue2 = PlayerPrefs.GetInt(Toggle2);
 
-        if (worldValue1 == 1) ToggleElements1.SetActive(true);
-        if (worldValue1 == 0) ToggleElements1.SetActive(false);
+        //if (worldValue1 == 1 && ToggleElements1 != null) ToggleElements1.SetActive(true);
+        //if (worldValue1 == 0 && ToggleElements1 != null) ToggleElements1.SetActive(false);
 
-        if (worldValue2 == 1) ToggleElements2.SetActive(true);
-        if (worldValue2 == 0) ToggleElements2.SetActive(false);
+        //if (worldValue2 == 1 && ToggleElements2 != null) ToggleElements2.SetActive(true);
+        //if (worldValue2 == 0 && ToggleElements2 != null) ToggleElements2.SetActive(false);
     }
 }
