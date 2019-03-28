205b8d52f6effc76d4e57b5e25c12e470240ef61
sean-turner1 <36117254+sean-turner1@users.noreply.github.com>
2019-03-28 01:28:15 +0000

    Triggers show borders based on distance to player

---
+++ Prototype/Prototype_clean/Assets/Scripts/TriggerBorder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBorder : MonoBehaviour
+{
+
+    GameObject player;
+    SpriteRenderer sprite;
+    Vector3 playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = player.transform.position;
+
+        float distance = Vector3.Distance(transform.position, playerPos);
+        sprite.color = new Color(255, 255, 255, (1f - (distance/6)));
+
+    }
+}
