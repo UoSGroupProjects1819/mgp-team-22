8e61c1cedba54c7d0210685421ea67425354dd3f
sean-turner1 <36117254+sean-turner1@users.noreply.github.com>
2019-04-02 00:29:16 +0100

    added world 3 artifact support in playerprefs

--- Prototype/Prototype_clean/Assets/Scripts/ArtifactListener.cs
+++ Prototype/Prototype_clean/Assets/Scripts/ArtifactListener.cs
@@ -6,18 +6,19 @@ public class ArtifactListener : MonoBehaviour
 
 
 {
-    public GameObject[] ToggleElementsOn1, ToggleElementsOff1, ToggleElementsOn2, ToggleElementsOff2;
-    private int worldValue1, worldValue2;
+    public GameObject[] ToggleElementsOn1, ToggleElementsOff1, ToggleElementsOn2, ToggleElementsOff2, ToggleElementsOn3, ToggleElementsOff3;
+    private int worldValue1, worldValue2, worldValue3;
 
     private Vector3 SpawnLocation;
 
-    public string Toggle1, Toggle2;
+    public string Toggle1, Toggle2, Toggle3;
 
     // Start is called before the first frame update
     void Start()
     {
         worldValue1 = PlayerPrefs.GetInt(Toggle1);
         worldValue2 = PlayerPrefs.GetInt(Toggle2);
+        worldValue3 = PlayerPrefs.GetInt(Toggle3);
 
         foreach (GameObject element in ToggleElementsOn1)
         {
@@ -33,14 +34,26 @@ public class ArtifactListener : MonoBehaviour
 
         foreach (GameObject element in ToggleElementsOn2)
         {
-            if (worldValue1 == 1) element.SetActive(true);
-            if (worldValue1 == 0) element.SetActive(false);
+            if (worldValue2 == 1) element.SetActive(true);
+            if (worldValue2 == 0) element.SetActive(false);
         }
 
         foreach (GameObject element in ToggleElementsOff2)
         {
-            if (worldValue1 == 1) element.SetActive(false);
-            if (worldValue1 == 0) element.SetActive(true);
+            if (worldValue2 == 1) element.SetActive(false);
+            if (worldValue2 == 0) element.SetActive(true);
+        }
+
+        foreach (GameObject element in ToggleElementsOn3)
+        {
+            if (worldValue3 == 1) element.SetActive(true);
+            if (worldValue3 == 0) element.SetActive(false);
+        }
+
+        foreach (GameObject element in ToggleElementsOff3)
+        {
+            if (worldValue3 == 1) element.SetActive(false);
+            if (worldValue3 == 0) element.SetActive(true);
         }
     }
 
