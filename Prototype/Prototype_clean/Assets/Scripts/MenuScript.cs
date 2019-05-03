28b500f90bc2bd8e205ddaa0ae2f06566b18a261
Joshua Donnell <43639286+Sawkss@users.noreply.github.com>
2019-05-03 07:25:55 +0100

    Working Main Menu

---
+++ Prototype/Prototype_clean/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("World1");
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+    public void ThirdBtn()
+    {
+
+
+    }
+}
